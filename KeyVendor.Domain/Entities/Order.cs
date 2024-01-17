e5390a4768c95b568d8aa1ac86a150335f960d59
Purgenta <onlinenidza352@gmail.com>
2024-01-17 20:18:20 +0100

    Added order entity, adjusted user so that it can be added as refference

---
+++ KeyVendor.Domain/Entities/Order.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.CompilerServices;
+using MongoDB.Entities;
+
+namespace KeyVendor.Domain.Entities;
+
+public class Order : BaseEntity
+{
+    public One<User> User { get; set; }
+    public Many<Key> Keys { get; set; }
+    public double TotalPrice { get; set; }
+    public OrderStatus Status { get; set; }
+
+    public One<User> Seller { get; set; }
+
+    public Order(double totalPrice, One<User> user, Many<Key> keys, One<User> seller)
+    {
+        this.TotalPrice = totalPrice;
+        this.User = user;
+        this.Keys = keys;
+        this.Status = OrderStatus.Pending;
+        this.Seller = seller;
+    }
+}
+
+public enum OrderStatus
+{
+    Pending,
+    Cancelled,
+    Completed
+}
--- KeyVendor.Domain/Entities/User.cs
+++ KeyVendor.Domain/Entities/User.cs
@@ -1,10 +1,25 @@
 ﻿using AspNetCore.Identity.MongoDbCore.Models;
+using MongoDB.Bson;
 using MongoDB.Entities;
 
 namespace KeyVendor.Domain.Entities;
 
 [Collection("user")]
-public class User : MongoIdentityUser<Guid>
+public class User : MongoIdentityUser<Guid>, IEntity
 {
     public double Money { get; set; }
+    public Many<Order> Orders { get; set; }
+
+    public string GenerateNewID()
+    {
+        return ObjectId.GenerateNewId().ToString();
+    }
+
+    public User(double money)
+    {
+        this.InitOneToMany(() => Orders);
+        this.Money = money;
+    }
+
+    public string? ID { get; set; }
 }
