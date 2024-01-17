6a0b1ef7580180dc669e4b90c52e32a8fb1a7e6b
Purgenta <onlinenidza352@gmail.com>
2024-01-17 23:01:33 +0100

    Added validation for creating an order

---
+++ KeyVendor.Application/Common/Validators/Order/CreateOrderDtoValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using KeyVendor.Application.Common.Dto.Order;
+
+namespace KeyVendor.Application.Common.Validators.Order;
+
+public class CreateOrderDtoValidator : AbstractValidator<CreateOrderDto>
+{
+    public CreateOrderDtoValidator()
+    {
+        RuleFor(x => x.Keys).NotNull().NotEmpty().Must(x => x.Count > 0);
+    }
+}
